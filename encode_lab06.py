5074d7c50d6f689906bf6da09e60eec7325cad44
Justyn Wang <jlwang4224@gmail.com>
2023-03-22 23:34:09 -0400

    Added decoder

--- encode_lab06.py
+++ encode_lab06.py
@@ -5,6 +5,17 @@ def encode_password(password):
         encoded_password += shifted_digit
     return encoded_password
 
+
+def decode_password(encoded_password):
+    flassword = [int(x) for a, x in enumerate(str(encoded_password))]
+    pass_word = [x - 3 if x > 3 else x + 7 for x in flassword]
+    decoded_password = ""
+    for y in pass_word:
+        decoded_password += str(y)
+        int(decoded_password)
+    return decoded_password
+
+
 while True:
     print("Menu")
     print("1. Encode")
@@ -16,7 +27,9 @@ while True:
         password = input("Please enter your password to encode: ")
         encoded_password = encode_password(password)
         print("Your password has been encoded and stored!")
-    
+    if decision == "2":
+        decoded_password = decode_password(encoded_password)
+        print("The encoded password is", encoded_password, "and the original password is", decoded_password)
 
 
 
